790a04334ea5ca794deef75ea00c0394779c09ee
Loubnaa1 <loubnamerzak4@gmail.com>
2023-07-27 02:44:55 +0200

    adding_stuctur

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ clear
 if [ "$1" = "--debug:build" ]; then
     gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fsanitize=address *.c -o build/shell
 elif [ "$1" = "--debug:run" ]; then
-    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -fsanitize=address *.c -o build/shell
+    gcc -Wall -Werror -Wextra -pedantic -std=gnu11 -fsanitize=address *.c -o build/shell
     if [ $? -eq 0 ]; then
         ./build/shell
     fi
